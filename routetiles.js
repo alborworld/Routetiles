1cec51215c40f6a747e9bff34888d49df85a5b8d
Alessandro Bortolussi <alessandro@alborworld.com>
2015-07-27 09:18:49 +0200

    Added generation of tiles

--- routetiles.js
+++ routetiles.js
@@ -1,22 +1,59 @@
 function getMinEnclosingRectangle(points) {
-  var xCoords = [];
-  var yCoords = [];
+  var lats = [];
+  var longs = [];
 
   for (var i = 0; i < points.length; i++) {
-    xCoords.push(points[i][0]);
-    yCoords.push(points[i][1]);
+    lats.push(points[i].lat);
+    longs.push(points[i].lon);
   }
 
-  var minX = Math.min.apply(Math, xCoords);
-  var minY = Math.min.apply(Math, yCoords);
-  var maxX = Math.max.apply(Math, xCoords);
-  var maxY = Math.max.apply(Math, yCoords);
+  var minLat = Math.min.apply(Math, lats);
+  var minLon = Math.min.apply(Math, longs);
+  var maxLat = Math.max.apply(Math, lats);
+  var maxLon = Math.max.apply(Math, longs);
 
-  return [[minX, minY], [maxX, maxY]];
+  return {min:{lat:minLat, lon:minLon}, max:{lat:maxLat, lon:maxLon}};
 }
 
-var p = [[1, 19], [22, 31], [13, 32], [14, 3]];
+// See http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+function long2tile(lon,zoom) { return (Math.floor((lon + 180)/360 * Math.pow(2, zoom))); }
+function lat2tile(lat,zoom)  { return (Math.floor((1 - Math.log(Math.tan(lat * Math.PI/180) + 1 / Math.cos(lat * Math.PI/180))/Math.PI)/2 * Math.pow(2, zoom))); }
+
+function downloadTile(zoom, tileX, tileY, path) {
+  var http = require('http');
+  var fs = require('fs');
+
+  var fileName =  tileX + "_" + tileY + "_" + zoom + ".png";
+
+  var file = fs.createWriteStream(path + fileName);
+  var request = http.get("http://tile.openstreetmap.org/" + zoom + "/" + tileX + "/" + tileY + ".png", function(response) {
+    response.pipe(file);
+  });
+}
+
+var p = [{lat:52.37965, lon:4.88658}, {lat:52.37645, lon:4.89474}, {lat:52.37404, lon:4.90023}, {lat:52.36890, lon:4.90577}];
+var zoom = 15;
+var path = "";
 
 var rectangle = getMinEnclosingRectangle(p);
 
 console.log(rectangle);
+
+var minX = lat2tile(rectangle.max.lat, zoom);
+var minY = long2tile(rectangle.min.lon, zoom);
+var maxX = lat2tile(rectangle.min.lat, zoom);
+var maxY = long2tile(rectangle.max.lon, zoom);
+
+console.log("minX: " + minX);
+console.log("minY: " + minY);
+console.log("maxX: " + maxX);
+console.log("maxY: " + maxY);
+
+for (var x = minX; x <= maxX; x++) {
+  for (var y = minY; y <= maxY; y++) {
+    console.log("x: " + x + ", y: " + y);
+    downloadTile(zoom, y, x, path);
+  }
+}
+
+//downloadTile(17, long2tile(13.37771496361961, 17), lat2tile(52.51628011262304, 17), "")
